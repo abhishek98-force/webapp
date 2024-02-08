a196d9bb2d3e4c89d52a3cc5b9fe512447dc1da2
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-02-07 20:35:34 -0500

    final commit

--- migrations/20240207170150-create-user.js
+++ migrations/20240207170150-create-user.js
@@ -1,9 +1,10 @@
 'use strict';
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
-      id: {
+      id: { 
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
