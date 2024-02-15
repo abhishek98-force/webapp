79ad49f3cc875e94e814934032ac01d6f921764f
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-02-14 20:14:13 -0500

    test changes

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -2,11 +2,23 @@ const app = require('../../server');
 const request = require('supertest');
 const db = require('../../models/index');
 
+
+
 describe('Integration test for user', () => {
     let first_name_given = "abhisjj";
     let last_name_given = "Doekkkkttt";
     let password_given = "skdjfhskdfjhg";
     let username_given = "bhavan12aabhishek@gmail.com";
+
+    beforeAll(async () => {
+        try {
+          await db.sequelize.sync();
+          console.log('Database synchronized');
+        } catch (error) {
+          console.error('Database synchronization error:', error);
+          process.exit(1); // Exit the process if synchronization fails
+        }
+      });
 
     afterAll(async () => {
         try{
@@ -23,7 +35,7 @@ describe('Integration test for user', () => {
       });
 
     it('insert user and check if it\'s inserted', async () => {
-       
+       console.log('inserting');
         const createUserResponse = await request(app)
             .post('/v1/user')
             .send({
@@ -34,7 +46,7 @@ describe('Integration test for user', () => {
             });
 
         expect(createUserResponse.status).toBe(201);
-
+        console.log('recieving');
         const getUserResponse = await request(app)
             .get('/v1/user/self')
             .auth(username_given, password_given);
