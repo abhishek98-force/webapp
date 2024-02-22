5b4dce383f572de00d748cf46aceba191b939799
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-02-22 17:02:45 -0500

    edit var

--- packer/scripts/script.sh
+++ packer/scripts/script.sh
@@ -18,7 +18,7 @@ sudo systemctl start postgresql
 
 cd /tmp
 
-#Create a PostgreSQL user called 'cent' edit 
+#Create a PostgreSQL user called 'cent' edi 
 sudo -u postgres psql -c "CREATE ROLE cent with LOGIN PASSWORD 'helloworld';"
 
 #Create a PostgreSQL database called 'testdb' owned by 'cent'
