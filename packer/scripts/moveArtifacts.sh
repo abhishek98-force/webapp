56a2ba7639bab4a9a6ac41a8d1c20f79227a2f32
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-02-27 10:51:23 -0500

    env file and permission edit

--- packer/scripts/moveArtifacts.sh
+++ packer/scripts/moveArtifacts.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+sudo groupadd csye6225
+sudo useradd -M -N -g csye6225 -s /usr/sbin/nologin csye6225
+sudo chown -R csye6225:csye6225 /home/packer/webapp
 sudo mkdir -p /home/csye6225
 sudo groupadd csye6225
 sudo useradd -M -N -g csye6225 -s /usr/sbin/nologin csye6225
 sudo chown -R csye6225:csye6225 /home/csye6225
 sudo mv /home/packer/webapp /home/csye6225
-sudo chown -R csye6225:cscredential_file_path
+sudo chown -R csye6225:csye6225 /home/csye6225/webapp
 
 sudo mv /tmp/node-server.service /etc/systemd/system/node-server.service
 sudo systemctl daemon-reload
