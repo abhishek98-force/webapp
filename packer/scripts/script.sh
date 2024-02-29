904552bfd9625e505c8384f1157df2b9b6cd010f
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-02-29 11:17:09 -0500

    update

--- packer/scripts/script.sh
+++ packer/scripts/script.sh
@@ -7,30 +7,8 @@ mkdir webapp
 #Install unzip
 sudo yum install -y unzip
 
-#Install PostgreSQL data
-sudo dnf -y install postgresql-server
-
-#Setup database
-sudo postgresql-setup --initdb
-
-#Start PostgreSQL service
-sudo systemctl start postgresql
-
 cd /tmp
 
-
-#Create a PostgreSQL user called 'cent' edi
-sudo -u postgres psql -c "CREATE ROLE csye6225 with LOGIN PASSWORD 'helloworld';"
-
-#Create a PostgreSQL database called 'testdb' owned by 'cent'
-sudo -u postgres createdb testdb --owner=csye6225
-
-echo "Please save and exit Vim to continue with the setup."
-
-sudo sed -i 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf
-
-#restart db
-sudo systemctl restart postgresql.service
 
 cd
 
