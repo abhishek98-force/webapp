e552f53058e069de0e7db6d73f768aab5286013c
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-03-18 19:07:02 -0400

    logger changes

---
+++ packer/scripts/install_ops_agent.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#create log directory
+sudo mkdir -p /var/log/csye6225
+sudo chown -R csye6225:csye6225 /var/log/csye6225
+
+# Download and install Ops Agent
+
+
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+# Enable Ops Agent service
+sudo systemctl enable google-cloud-ops-agent
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+var winston = require('winston'),
+    expressWinston = require('express-winston');
 const db = require('./models');;
 const port = process.env.PORT || 8000;
 
@@ -9,15 +11,64 @@ const health = require('./src/controllers/health');
 
 
 app.use(express.json());
+
+app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.File({
+        filename : '/var/log/csye6225/webapp.log',
+      })
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json(),
+      winston.format.prettyPrint()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+  }));
+
+
 app.use('/v1/user', user);
 app.get('/healthz', health.healthCheck);
 app.all('/healthz', async (req, res) => {
     res.status(405).header('Cache-Control','no-cache').send();
 })
 
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.File({
+          filename : '/var/log/csye6226/errorWebapp.log',
+        })
+      ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json(),
+      winston.format.prettyPrint()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+
+  }));
+
+app.use((err, req, res, next) => {
+    console.log("Middleware Error Hadnling");
+    const errStatus = err.statusCode || 500;
+    const errMsg = err.message || 'Something went wrong';
+    console.log(errMsg);
+    res.status(400).send(errMsg);
+});
+
+
+
 app.use((req, res, next) => {
     res.status(404).send();
-})
+})  
 
 
 
