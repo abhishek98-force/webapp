e5c6b57979c242004ba7696c1c9ad3053a770790
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-03-20 13:39:09 -0400

    small change

--- packer/scripts/moveArtifacts.sh
+++ packer/scripts/moveArtifacts.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-sudo groupadd csye6225
+
 sudo useradd -M -N -g csye6225 -s /usr/sbin/nologin csye6225
-sudo chown -R csye6225:csye6225 /home/packer/webapp
 sudo mkdir -p /home/csye6225
 sudo groupadd csye6225
-sudo useradd -M -N -g csye6225 -s /usr/sbin/nologin csye6225
-sudo chown -R csye6225:csye6225 /home/csye6225
 sudo mv /home/packer/webapp /home/csye6225
-sudo chown -R csye6225:csye6225 /home/csye6225/webapp
+
+sudo mkdir -p /etc/csye6225
 
 sudo mv /tmp/node-server.service /etc/systemd/system/node-server.service
-sudo systemctl daemon-reload
-sudo systemctl enable node-server.service
+
+
 
 
