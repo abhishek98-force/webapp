db29aca27d8d02a320188c53d2ec846c067a395f
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-03-20 15:26:58 -0400

    change order

--- packer/scripts/moveArtifacts.sh
+++ packer/scripts/moveArtifacts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
+sudo groupadd csye6225
 sudo useradd -M -N -g csye6225 -s /usr/sbin/nologin csye6225
 sudo mkdir -p /home/csye6225
-sudo groupadd csye6225
+
 sudo mv /home/packer/webapp /home/csye6225
 
 sudo mkdir -p /etc/csye6225
