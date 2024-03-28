99ed9442d0ff92e76b95b4f8c6987d83b1d1b5a4
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2024-03-28 12:24:44 -0400

    update

--- src/routes/verificationRoute.js
+++ src/routes/verificationRoute.js
@@ -14,17 +14,17 @@ router.get('/:token', async (req, res) => {
   
       if (!user) {
         webappLogger.error("user not found");
-        return res.status(404).send();
+        return res.status(404).send('<h1>User not found</h1>');
       }
 
       if (user.tokenExpiration && new Date() > user.tokenExpiration) {
         webappLogger.error("token expired");
-        return res.status(400).send();
+        return res.status(400).send('<h1>Verification failed: token expired</h1>');
       }
   
       user.isVerified = true;
       await user.save();
-      res.status(200).send();
+      res.status(200).send('<h1>User verified successfully</h1>');
     } catch (error) {
       console.error(error);
       webappLogger.error("error"+error);
